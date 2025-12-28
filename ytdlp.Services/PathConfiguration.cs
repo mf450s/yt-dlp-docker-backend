e304347185f895fd07bb9c5204b6903e97deb97b
Tom Becker <tomdastulpenlakritz@gmail.com>
2025-12-28 23:57:22 +0100

    corrected paths

--- ytdlp.Services/PathConfiguration.cs
+++ ytdlp.Services/PathConfiguration.cs
@@ -1,7 +1,8 @@
 namespace ytdlp.Configs;
 public class PathConfiguration
 {
-    public string Downloads { get; set; } = "/app/downloads/";
-    public string Archive { get; set; } = "/app/archive/";
-    public string Config { get; set; } = "/app/configs/";
+    // must be without closing / 
+    public string Downloads { get; set; } = "/app/downloads";
+    public string Archive { get; set; } = "/app/archive";
+    public string Config { get; set; } = "/app/configs";
 }
