9a8e01909088a2bd5b2daa84a8f08b1b3af6eef9
Tom Becker <tomdastulpenlakritz@gmail.com>
2025-12-30 03:43:26 +0100

    fix: Add missing slash in Cookies path in PathConfiguration.cs

--- ytdlp.Services/PathConfiguration.cs
+++ ytdlp.Services/PathConfiguration.cs
@@ -5,5 +5,5 @@ public class PathConfiguration
     public string Downloads { get; set; } = "/app/downloads/";
     public string Archive { get; set; } = "/app/archive/";
     public string Config { get; set; } = "/app/configs/";
-    public string Cookies { get; set; } = "/app/cookies/";
+    public string Cookies { get; set; } = "/app/cookies//";
 }
