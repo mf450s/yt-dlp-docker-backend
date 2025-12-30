35b42acc9549bc9ae9a60743f6de6a67a00f9ad9
mf450s <85843054+mf450s@users.noreply.github.com>
2025-12-30 01:41:09 +0100

    refactor: update IDownloadingService interface - cookies via config only

--- ytdlp.Services/Interfaces/IDownloadingService.cs
+++ ytdlp.Services/Interfaces/IDownloadingService.cs
@@ -3,11 +3,11 @@ namespace ytdlp.Services.Interfaces;
 public interface IDownloadingService
 {
     /// <summary>
-    /// Attempts to download content from a URL using the specified configuration and optional cookie file.
+    /// Attempts to download content from a URL using the specified configuration.
+    /// Cookie files should be specified within the config file using --cookies option.
     /// </summary>
     /// <param name="url">The URL to download from.</param>
     /// <param name="configFile">The name of the configuration file to use.</param>
-    /// <param name="cookieFile">Optional: The name of the cookie file to use for authentication.</param>
     /// <returns>A task representing the asynchronous download operation.</returns>
-    Task TryDownloadingFromURL(string url, string configFile, string? cookieFile = null);
+    Task TryDownloadingFromURL(string url, string configFile);
 }
