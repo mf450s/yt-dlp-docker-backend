1f913b9e029be3aeb8571aa0f3bcce43d44eb6c9
Tom Becker <tomdastulpenlakritz@gmail.com>
2026-01-08 13:55:27 +0100

    style: Remove unused using directive and comment

--- ytdlp.Services/Interfaces/IPathParserService.cs
+++ ytdlp.Services/Interfaces/IPathParserService.cs
@@ -1,13 +1,6 @@
-using System;
-
 namespace ytdlp.Services.Interfaces;
 
 public interface IPathParserService
 {
     string CheckAndFixPaths(string line);
-    
-    /// <summary>
-    /// Gets the cookies folder path from the PathConfiguration.
-    /// </summary>
-    /// <returns>The full path to the cookies folder.</returns>
 }
